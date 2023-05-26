a6ab9e1a6e43b194be1f6240287cf1aa6e8fbb15
ordinaldev <121743907+ordinaldev@users.noreply.github.com>
2023-05-26 16:07:42 -0700

    Update postingan.js

--- assets/js/postingan.js
+++ assets/js/postingan.js
@@ -3,7 +3,7 @@ var judul = [
     'Chrome atau Edge ? Mana yang lebih baik ? ', 'My Frontend Starter Template V1'
 ]
 var link = [
-    './post/chromevsedge',
+    './post/chromevsedge','https://github.com/Zero-Spectre/mystartertemplate'
 ]
 var gambar = [
     './assets/images/post/1.png', './assets/images/post/2.png',
